509233476aef3e85f621effc64ed11dd89461cda
falgune2701 <falgunemondal2@gmail.com>
2025-03-27 16:56:13 +0530

    Messages are given based on users BMI

--- projects/project2/script.js
+++ projects/project2/script.js
@@ -3,7 +3,8 @@ form.addEventListener('submit', function (e){
 e.preventDefault()  // stop  by default action
 const height = parseInt(document.querySelector('#height').value);
 const weight = parseInt(document.querySelector('#weight').value);
-const result = document.querySelector('#result')
+const result = document.querySelector('#result');
+const user_sms = document.querySelector('#user-sms');
 if (height === '' || height < 0 || isNaN(height)) {
     result.innerHTML = 'please give a vaild height!';
 } else if (weight === '' || weight<0 || isNaN(weight)) {
@@ -11,6 +12,13 @@ if (height === '' || height < 0 || isNaN(height)) {
 } else{
     // show result
     const bmi = (weight/((height*height)/10000)).toFixed(2);
-   result.innerHTML = `<span>${bmi}</span>`;
+   result.innerHTML = `<span> BMI = ${bmi}</span>`;
+   if (bmi<18.6) {
+    user_sms.innerHTML = "Under Weight";
+   } else if (bmi>18.6 && bmi<24.9){
+    user_sms.innerHTML = "Normal Range";
+   } else {
+    user_sms.innerHTML = "Overweight";
+   }
 }
 })
